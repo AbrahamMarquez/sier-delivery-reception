c5045cfa42efbd13d81c05984add87cfca93e5b3
unknown <mario.sida.ramirez@gmail.com>
2024-07-13 13:04:56 -0600

    cambios en tlajomulco pagina

--- src/components/organisms/Landing/Landing.tsx
+++ src/components/organisms/Landing/Landing.tsx
@@ -4,6 +4,10 @@ import imgOval1 from '@/../public/images/landing/Oval.svg'
 import imgOval2 from '@/../public/images/landing/Oval(1).svg'
 import imgGreen from '@/../public/images/landing/Rectangle1.svg'
 import imgCheckGreen from '@/../public/images/landing/Icon(23).svg'
+import imgOc from '@/../public/images/landing/Rectangle.svg'
+import imgOc1 from '@/../public/images/landing/Rectangle(1).svg'
+import imgOc2 from '@/../public/images/landing/Rectangle(2).svg'
+import imgOc3 from '@/../public/images/landing/Rectangle(3).svg'
 
 import Image from 'next/image'
 
@@ -121,12 +125,120 @@ export function Landing(): React.JSX.Element {
             <div className={styles.textBlack}>Comisario de la Policía preventiva municipal</div>
           </div>
         </div>
-        <div className={styles.div3}></div>
-        <div className={styles.div4}></div>
-        <div className={styles.div5}></div>
-        <div className={styles.div6}></div>
+        <div className={styles.div3}>
+          <Image
+            src={imgOc}
+            alt='ball1'
+            className={styles.imgFloat1}
+          />
+          <Image
+            src={imgOc1}
+            alt='ball1'
+            className={styles.imgFloat2}
+          />
+        </div>
+        <div className={styles.div4}>
+          <Image
+            src={imgOc2}
+            alt='ball1'
+            className={styles.imgFloat1}
+          />
+          <Image
+            src={imgOc3}
+            alt='ball1'
+            className={styles.imgFloat2}
+          />
+        </div>
+        <div className={styles.div5}>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>Fiscal ambiental de Tlajomulco</div>
+          </div>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>Coordinadores Generales</div>
+          </div>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>Directores Generales</div>
+          </div>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>secretario particular</div>
+          </div>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>los directores de Área</div>
+          </div>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>delegados y agentes municipales</div>
+          </div>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>jefes de departamento</div>
+          </div>
+        </div>
+        <div className={styles.div6}>
+          <div className={styles.displayFlex}>
+            <Image
+              src={imgCheckGreen}
+              alt='ball1'
+              className={styles.img}
+            />
+            <div className={styles.textBlack}>
+              Y demás mandos medios relacionados con el manejo y/o control de bienes financieros o patrimoniales, así como los Titulares de los Organismos Públicos Descentralizados del Municipio
+            </div>
+          </div>
+        </div>
       </div>
-      <div></div>
+      <div className={styles.colorBalls}>
+        <div className={styles.div1}>
+          <div className={styles.divYellow}></div>
+          <div className={styles.text}>Normatividad</div>
+        </div>
+        <div className={styles.textBig}>
+          <span>El Artículo 20 de la Ley de Entrega-Recepción del Estado de Jalisco</span> es clave porque establece las responsabilidades y obligaciones de los servidores públicos salientes en el proceso de entrega-recepción de su gestión. 
+        </div>
+        <div className={styles.textBig + ' ' + styles.pt0}>
+          Aquí están los puntos más importantes:
+        </div>
+        <div className={styles.colors_container}>
+          <div className={styles.green}></div>
+          <div className={styles.red}></div>
+          <div className={styles.yellow}></div>
+          <div className={styles.blue}></div>
+          <div className={styles.grey}></div>
+        </div>
+      </div>
       <div></div>
       <div></div>
     </div>
